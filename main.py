fc4c9a4d379eb3c9be1d5f821558ba9078d13f78
ONEELEVEN228 <oneeleven@ukr.net>
2025-07-11 04:24:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging, openai, telebot
 import os
 
-# Налаштування
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
